ebe82e6e70cedc52b4e7d13fe168a4304f3c5b13
Antony Bailey <antonybailey@Antonys-MacBook-Pro.local>
2025-03-24 14:55:05 +0000

    this is getting somewhere finally

--- pages/resources.php
+++ pages/resources.php
@@ -16,46 +16,107 @@ $pageDescription = "Access a comprehensive collection of Houses on the Air resou
 
     <p class="flow-text">Find everything you need for successful Houses on the Air operations, from beginner guides to technical references.</p>
 
-    <!-- Featured Resources -->
+    <!-- Most Popular Resources - Highlighting House Activations -->
+    <div class="card blue-grey lighten-5">
+        <div class="card-content">
+            <span class="card-title">Essential HOTA Resources</span>
+            <div class="row">
+                <div class="col s12 m6">
+                    <div class="card-panel deep-orange lighten-5">
+                        <h4><i class="material-icons left medium">home</i>House Activation Guide</h4>
+                        <p class="flow-text">Everything you need to know about successfully activating houses for HOTA.</p>
+                        <ul class="browser-default">
+                            <li>Learn what qualifies as a valid activation</li>
+                            <li>Equipment recommendations and setup tips</li>
+                            <li>Logging requirements and best practices</li>
+                        </ul>
+                        <div class="center-align mt-3">
+                            <a href="?page=house-activations" class="btn-large deep-orange lighten-1 waves-effect waves-light">
+                                <i class="material-icons left">arrow_forward</i>Read the Activation Guide
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col s12 m6">
+                    <div class="card-panel blue lighten-5">
+                        <h4><i class="material-icons left medium">search</i>Hunter's Guide</h4>
+                        <p class="flow-text">Learn how to find and contact HOTA activations.</p>
+                        <ul class="browser-default">
+                            <li>Recommended frequencies and calling procedures</li>
+                            <li>Logging requirements for award claims</li>
+                            <li>Tips for finding rare house activations</li>
+                        </ul>
+                        <div class="center-align mt-3">
+                            <a href="?page=participate#hunter" class="btn-large blue lighten-1 waves-effect waves-light">
+                                <i class="material-icons left">arrow_forward</i>Hunter's Guide
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Featured Resources - IMPROVED DISPLAY -->
+    <h3>Featured Resources</h3>
     <div class="row">
         <div class="col s12 m4">
-            <div class="card">
-                <div class="card-image">
-                    <span class="card-title">House Activation Guide</span>
+            <div class="card featured-card">
+                <div class="card-image blue lighten-4">
+                    <i class="material-icons large center-icon">description</i>
+                    <span class="card-title">Logging & Submissions</span>
                 </div>
                 <div class="card-content">
-                    <p>Learn how to successfully activate houses for HOTA with our step-by-step guide to equipment, planning, and operation.</p>
+                    <p>Learn how to properly format and submit your logs for HOTA award credit and activations. Proper logging is essential for HOTA participation.</p>
+                    <ul class="browser-default resource-list">
+                        <li><a href="?page=log-entry">Log Submission Process</a></li>
+                        <li><a href="?page=adif-guide">ADIF Format Guide</a></li>
+                        <li><a href="?page=logging-software">Logging Software Options</a></li>
+                    </ul>
                 </div>
                 <div class="card-action">
-                    <a href="?page=house-activations">Read the Guide</a>
+                    <a href="?page=log-entry" class="btn blue waves-effect waves-light">Submit Your Logs</a>
                 </div>
             </div>
         </div>
 
         <div class="col s12 m4">
-            <div class="card">
-                <div class="card-image">
-                    <span class="card-title">ADIF Guide</span>
+            <div class="card featured-card">
+                <div class="card-image green lighten-4">
+                    <i class="material-icons large center-icon">settings_input_antenna</i>
+                    <span class="card-title">Portable Equipment</span>
                 </div>
                 <div class="card-content">
-                    <p>Understand the Amateur Data Interchange Format (ADIF) and learn how to properly format your logs for HOTA submissions.</p>
+                    <p>Discover the best portable antenna options and equipment for successful HOTA activations. Find solutions for different locations and operating styles.</p>
+                    <ul class="browser-default resource-list">
+                        <li>Wire antenna solutions</li>
+                        <li>Vertical and portable options</li>
+                        <li>Power sources and accessories</li>
+                    </ul>
                 </div>
                 <div class="card-action">
-                    <a href="?page=adif-guide">Read the Guide</a>
+                    <a href="?page=portable-antennas" class="btn green waves-effect waves-light">Portable Antenna Guide</a>
                 </div>
             </div>
         </div>
 
         <div class="col s12 m4">
-            <div class="card">
-                <div class="card-image">
-                    <span class="card-title">Portable Antennas</span>
+            <div class="card featured-card">
+                <div class="card-image purple lighten-4">
+                    <i class="material-icons large center-icon">card_membership</i>
+                    <span class="card-title">Get Licenced</span>
                 </div>
                 <div class="card-content">
-                    <p>Discover the best portable antenna options for HOTA activations, from quick-deploy dipoles to compact magnetic loops.</p>
+                    <p>Resources for obtaining your amateur radio licence so you can fully participate in HOTA. Find guidance for exams and licensing requirements.</p>
+                    <ul class="browser-default resource-list">
+                        <li>UK licensing requirements</li>
+                        <li>Study materials and practice tests</li>
+                        <li>International licensing information</li>
+                    </ul>
                 </div>
                 <div class="card-action">
-                    <a href="?page=portable-antennas">View Antenna Solutions</a>
+                    <a href="?page=get-licenced" class="btn purple waves-effect waves-light">Licensing Guide</a>
+                    <a href="?page=online-training" class="btn-flat">Training Resources</a>
                 </div>
             </div>
         </div>
@@ -126,6 +187,39 @@ $pageDescription = "Access a comprehensive collection of Houses on the Air resou
         </div>
     </div>
 
+    <!-- Activator Resources Section - New dedicated section for activators -->
+    <div class="card green lighten-5">
+        <div class="card-content">
+            <span class="card-title"><i class="material-icons left">cell_tower</i>Activator Resources</span>
+            <p>Comprehensive guides and tools for HOTA activators:</p>
+
+            <div class="row">
+                <div class="col s12 m6">
+                    <h5>Activation Guides</h5>
+                    <ul class="browser-default">
+                        <li><strong><a href="?page=house-activations">Complete House Activation Guide</a></strong> - Essential reading for all activators</li>
+                        <li><a href="?page=operating-guidelines">HOTA Operating Guidelines</a> - Best practices for on-air operation</li>
+                        <li><a href="?page=portable-antennas">Portable Antenna Solutions</a> - Antenna options for different locations</li>
+                    </ul>
+                </div>
+                <div class="col s12 m6">
+                    <h5>Activation Tools</h5>
+                    <ul class="browser-default">
+                        <li><a href="?page=log-entry">Log Submission for Activators</a> - How to submit your activations</li>
+                        <li><a href="?page=band-plans">Band Plans & Frequencies</a> - Recommended HOTA frequencies</li>
+                        <li><a href="?page=contests">Contest & Event Calendar</a> - Special activation opportunities</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="center-align mt-3">
+                <a href="?page=house-activations" class="btn green darken-1 waves-effect waves-light">
+                    <i class="material-icons left">home</i>House Activation Guide
+                </a>
+            </div>
+        </div>
+    </div>
+
     <!-- Training & Educational Resources -->
     <div class="card">
         <div class="card-content">
@@ -155,7 +249,7 @@ $pageDescription = "Access a comprehensive collection of Houses on the Air resou
                         <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/VIDEO_ID_1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen rel="nofollow"></iframe>
                     </div>
                     <h5>HOTA Introduction</h5>
-                    <p>A quick overview of the Houses on the Air program and how to participate.</p>
+                    <p>A quick overview of the Houses on the Air programme and how to participate.</p>
                 </div>
                 <div class="col s12 m6">
                     <div class="video-container">
@@ -231,5 +325,107 @@ $pageDescription = "Access a comprehensive collection of Houses on the Air resou
     left: 0;
     width: 100%;
     height: 100%;
+}
+
+.mt-3 {
+    margin-top: 1.5rem;
+}
+
+/* Highlight essential links */
+.card-panel.deep-orange.lighten-5 h4,
+.card-panel.blue.lighten-5 h4 {
+    margin-top: 0;
+    font-weight: 500;
+}
+
+.card-panel.deep-orange.lighten-5,
+.card-panel.blue.lighten-5 {
+    border-left: 5px solid;
+}
+
+.card-panel.deep-orange.lighten-5 {
+    border-color: #ff5722;
+}
+
+.card-panel.blue.lighten-5 {
+    border-color: #2196f3;
+}
+
+/* IMPROVED STYLING FOR FEATURED CARDS */
+.featured-card {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    transition: transform 0.2s, box-shadow 0.2s;
+}
+
+.featured-card:hover {
+    transform: translateY(-5px);
+    box-shadow: 0 8px 17px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
+}
+
+.featured-card .card-image {
+    position: relative;
+    padding: 20px 0;
+    text-align: center;
+    height: 120px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+
+.featured-card .card-title {
+    position: relative;
+    padding: 0 24px;
+    color: rgba(0,0,0,0.87);
+    width: 100%;
+    text-align: center;
+    margin-top: 0;
+}
+
+.featured-card .card-content {
+    flex-grow: 1;
+    padding-bottom: 16px;
+}
+
+.featured-card .card-action {
+    border-top: 1px solid rgba(0,0,0,0.1);
+    padding: 16px 24px;
+}
+
+.center-icon {
+    font-size: 48px !important;
+    display: block;
+    margin: 0 auto 8px auto;
+    color: rgba(0,0,0,0.6);
+}
+
+.resource-list {
+    margin-top: 15px;
+    margin-bottom: 0;
+}
+
+.resource-list li {
+    margin-bottom: 8px;
+}
+
+.resource-list li:last-child {
+    margin-bottom: 0;
+}
+
+/* Responsive adjustments */
+@media only screen and (max-width: 600px) {
+    .featured-card {
+        margin-bottom: 20px;
+    }
+
+    .featured-card .card-action {
+        text-align: center;
+    }
+
+    .featured-card .btn {
+        width: 100%;
+        margin-bottom: 8px;
+    }
 }
 </style>
